10d68f5e78b22bc87ce608b64ea6d224957dbd07
Ezekiel <ezekieldsanchez@gmail.com>
2024-08-11 04:20:10 +0800

    Recreated portfolio

---
+++ landingpage.js
@@ -0,0 +1,29 @@
+const topContainer = `
+    <div id="top-container">
+        <div id="top-upper-container">
+            <button
+                class="header-btn"
+                id="about-btn"
+                type="button"
+                title="Get to know the games cast.">
+                ABOUT
+            </button>
+            <button
+                class="header-btn"
+                id="cast-btn"
+                type="button"
+                title="Get to know about the game.">
+                CAST
+            </button>
+        </div>
+        <div id="top-bottom-container">
+            <button
+                class="header-btn"
+                id="download-btn"
+                type="button"
+                title="Download the game.">
+                DOWNLOAD
+            </button>
+        </div>
+    </div>
+      `;
